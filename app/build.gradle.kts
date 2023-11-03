885f0f3222d21b0c5c29f551937f4cbf1b022d7b
mvidalt <mvidalt@cifpfbmoll.eu>
2023-11-03 13:40:41 +0100

    Task 12A completado

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.example.hellotoast"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.hellotoast"
         minSdk = 26
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
